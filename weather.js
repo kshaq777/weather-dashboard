438acdc204c4d8be6aec06fea815232698500fe7
kshaq777 <kshaq777@gmail.com>
2020-10-24 17:51:45 -0400

    functionality for city search, one-call api call, and current weather display

---
+++ weather.js
@@ -0,0 +1,7 @@
+$('#getWeather').on('click', function(e) {
+    e.preventdefault();
+    queryURL = 'api.openweathermap.org/data/2.5/forecast?q=trenton}&appid=c406a5e75ec8149f72f933aa5f754ef9'
+
+    $.ajax
+
+})
